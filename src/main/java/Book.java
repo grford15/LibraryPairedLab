2d78f3af5d85ac82f6a25bc83e243e1ebec7355f
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-11 13:10:40 +0100

    add book contructor and Genre enum

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,17 @@
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private boolean onLoan;
+    private Borrower currentBorrower;
+    private Genre genre;
+
+    public Book(String title, String author, boolean onLoan, Borrower currentBorrower, Genre genre) {
+        this.title = title;
+        this.author = author;
+        this.onLoan = onLoan;
+        this.currentBorrower = currentBorrower;
+        this.genre = genre;
+    }
 }
